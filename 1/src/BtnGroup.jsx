1a79abffe6c70bc344a5078be4c7db273cb63812
Максим <160880502+elow1111@users.noreply.github.com>
2024-10-10 21:53:27 +0300

    BtnGroup.jsx

--- 1/src/BtnGroup.jsx
+++ 1/src/BtnGroup.jsx
@@ -3,7 +3,30 @@ import React, { useState } from 'react';
 
 const BtnGroup = () => {
   // BEGIN (write your solution here)
+  const [activeButton, setActiveButton] = useState(null);
 
+  const handleClick = (button) => {
+    setActiveButton(button);
+  };
+
+  return (
+    <div className="btn-group" role="group">
+      <button
+        type="button"
+        className={cn('btn', 'btn-secondary', 'left', { active: activeButton === 'left' })}
+        onClick={() => handleClick('left')}
+      >
+        Left
+      </button>
+      <button
+        type="button"
+        className={cn('btn', 'btn-secondary', 'right', { active: activeButton === 'right' })}
+        onClick={() => handleClick('right')}
+      >
+        Right
+      </button>
+    </div>
+  );
   // END
 };
 
